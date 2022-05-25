3daded985bf132e421fb5d45680fb96dfae9c5e6
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-25 11:16:30 +0530

    chore:userList component updates.

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -12,7 +12,12 @@ const UsersList = () => {
 						<div key={user._id.toString()} className=''>
 							<div className='font-bold'>{user.name}</div>
 							<div>Age: {user.age}</div>
+							<div>Birth: {user.birth}</div>
+							<div>BMI: {user.bmi}</div>
+							<div>Gender: {user.gender}</div>
 							<div> Height:{user.height}</div>
+							<div>Ideal Weight: {user.idealWeight}</div>
+							<div>Weight: {user.weight}</div>
 						</div>
 					)
 				})}
