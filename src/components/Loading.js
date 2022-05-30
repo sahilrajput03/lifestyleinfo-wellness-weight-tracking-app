55bf2da2dff4bc29d6a8f35373b4961f0bd78ef9
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-30 11:50:49 +0530

    Add boy exercise loading gif.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,4 +1,13 @@
 const Loading = () => {
-	return <img className='m-auto' src='https://i.giphy.com/media/kUTME7ABmhYg5J3psM/giphy.webp' alt='Loading gif' />
+	// simple fidgeting
+	// return <img className='m-auto' src='https://i.giphy.com/media/kUTME7ABmhYg5J3psM/giphy.webp' alt='Loading gif' />
+
+	// boy exercising
+	return (
+		<>
+			<img className='m-auto w-40 aspect-auto' src='https://i.giphy.com/media/JRDDRoGhHexZ3Bcj0c/giphy.webp' alt='Loading gif' />
+			<h1>We make the world happier and healthier!</h1>
+		</>
+	)
 }
 export default Loading
