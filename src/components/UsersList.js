d844b6ce4aa3c750164fa78833378ee23eccdf6c
Sahil Rajput <sahilrajput03@gmail.com>
2022-05-30 10:27:13 +0530

    Add loading bar to UsersList page as well.

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import useUsersContext from '../contexts/useUsersContext'
 import useRefetchUsers from '../hooks/useRefetchUsers'
+import Loading from './Loading'
 
 const UsersList = () => {
 	const [users] = useUsersContext()
@@ -10,43 +11,48 @@ const UsersList = () => {
 	return (
 		<div className='card-center'>
 			<h1>Users</h1>
-			<div className='flex-col justify-center'>
-				{users &&
-					users.map((user) => {
-						const deleteUser = async () => {
-							let userMongo = window.userMongo
+			{users ? (
+				<>
+					<div className='flex-col justify-center'>
+						{users.map((user) => {
+							const deleteUser = async () => {
+								let userMongo = window.userMongo
 
-							await userMongo.functions.deleteUser(user._id.toString())
-							await refetchUsers()
-							alert('User Deleted!')
-						}
+								await userMongo.functions.deleteUser(user._id.toString())
+								await refetchUsers()
+								alert('User Deleted!')
+							}
 
-						return (
-							<div key={user._id.toString()} className='flex mb-5'>
-								<div className='mr-10'>
-									<div className='font-bold'>{user.name}</div>
-									<div>Age: {user.age}</div>
-									<div>Birth: {user.birth}</div>
-									<div>Gender: {user.gender}</div>
-									<button className='btn-secondary btn-red py-0 px-2' onClick={deleteUser}>
-										Delete User
-									</button>
+							return (
+								<div key={user._id.toString()} className='flex mb-5'>
+									<div className='mr-10'>
+										<div className='font-bold'>{user.name}</div>
+										<div>Age: {user.age}</div>
+										<div>Birth: {user.birth}</div>
+										<div>Gender: {user.gender}</div>
+										<button className='btn-secondary btn-red py-0 px-2' onClick={deleteUser}>
+											Delete User
+										</button>
+									</div>
+									<div>
+										<div>Height:{user.height}</div>
+										<div>Weight: {user.weight}</div>
+										<div>Ideal Weight: {user.idealWeight}</div>
+										<div>BMI: {user.bmi}</div>
+									</div>
 								</div>
-								<div>
-									<div>Height:{user.height}</div>
-									<div>Weight: {user.weight}</div>
-									<div>Ideal Weight: {user.idealWeight}</div>
-									<div>BMI: {user.bmi}</div>
-								</div>
-							</div>
-						)
-					})}
-			</div>
-			{debug ? <pre>{JSON.stringify(users, null, 2)}</pre> : null}
-			<br />
-			<div className='flex justify-end'>
-				<button onClick={() => setDebug(!debug)}>Debug?</button>
-			</div>
+							)
+						})}
+					</div>
+					{debug ? <pre>{JSON.stringify(users, null, 2)}</pre> : null}
+					<br />
+					<div className='flex justify-end'>
+						<button onClick={() => setDebug(!debug)}>Debug?</button>
+					</div>
+				</>
+			) : (
+				<Loading />
+			)}
 		</div>
 	)
 }
