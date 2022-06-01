77f7b05404f7ee665c7431b61e349768dc2e69f7
Sahil Rajput <sahilrajput03@gmail.com>
2022-06-01 11:47:37 +0530

    BUILD VIA `deploy` SCRIPT.

--- src/components/InstallPWA.js
+++ src/components/InstallPWA.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
+// src: https://dev.to/woile/simplest-react-hook-component-for-pwa-install-button-2die
 const InstallPWA = () => {
 	const [supportsPWA, setSupportsPWA] = useState(false)
 	const [promptInstall, setPromptInstall] = useState(null)
@@ -7,6 +8,7 @@ const InstallPWA = () => {
 	useEffect(() => {
 		const handler = (e) => {
 			e.preventDefault()
+			alert('ye got here.')
 			console.log('we are being triggered :D')
 			setSupportsPWA(true)
 			setPromptInstall(e)
@@ -34,3 +36,8 @@ const InstallPWA = () => {
 }
 
 export default InstallPWA
+
+// for more pwa hook: https://dev.to/rikurouvila/react-hook-for-showing-custom-add-to-home-screen-prompt-472c
+// https://web.dev/learn/pwa/installation-prompt/
+// amazing docs @ mdn: https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
+// pwa react dpcs: https://create-react-app.dev/docs/making-a-progressive-web-app/
