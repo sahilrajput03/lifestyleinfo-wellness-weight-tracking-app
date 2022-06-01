1ec902131ff2ec5c33dda65ae162b9db251c5d43
Sahil Rajput <sahilrajput03@gmail.com>
2022-06-01 14:03:11 +0530

    BUILD VIA `deploy` SCRIPT.

--- src/components/InstallPWA.js
+++ src/components/InstallPWA.js
@@ -4,8 +4,15 @@ import React, {useEffect, useState} from 'react'
 const InstallPWA = () => {
 	const [supportsPWA, setSupportsPWA] = useState(false)
 	const [promptInstall, setPromptInstall] = useState(null)
+	const [installed, setInstalled] = useState(null)
 
 	useEffect(() => {
+		// My own way to to check if user has pwa app installed or not!
+		if (window.matchMedia('(display-mode: standalone)').matches) {
+			// src: https://stackoverflow.com/a/51735941/10012446
+			setInstalled(true)
+		}
+
 		const handler = (e) => {
 			e.preventDefault()
 			// console.log('::beforeinstallprompt::we are being triggered :D')
@@ -17,12 +24,31 @@ const InstallPWA = () => {
 		return () => window.removeEventListener('transitionend', handler)
 	}, [])
 
-	const onClick = (evt) => {
+	const onClick = async (evt) => {
 		evt.preventDefault() // disable default info bar install prompt shown in chrome android and desktop.
 		if (!promptInstall) return
 
 		promptInstall.prompt()
+
+		let {outcome} = await promptInstall.userChoice
+		// alert(outcome)
+		if (outcome === 'accepted') {
+			// console.log('User accepted the A2HS prompt')
+			alert('Installing.. Please wait for few seconds..') // to be replaced with better ui later on.. // todo
+			// todo: refresh app after 7 seconds and show the banner that you have this website as app installed somehow..
+
+			// Reloading in case for desktop browser works real good as it will hide th Install button asap now that the user has chosen to install the app. ~ Sahil
+			window.document.location.reload()
+		} else {
+			alert('User dismissed the A2HS prompt')
+		}
 	}
+
+	if (installed) {
+		// for mobile can I redirect the user to pwa with this?
+		return 'Note: You have already `New Life Style Wellness Centre App` installed, you can use that to get a good experience.'
+	}
+
 	if (!supportsPWA) {
 		return null
 	}
