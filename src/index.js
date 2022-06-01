c95e1841f7857e7dfad346e170588091b6a772ec
Sahil Rajput <sahilrajput03@gmail.com>
2022-06-01 11:07:33 +0530

    BUILD VIA `deploy` SCRIPT.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 import AppRoutes from './AppRoutes'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 window.addEventListener('beforeinstallprompt', async (e) => {
@@ -56,11 +55,6 @@ root.render(
 		<AppRoutes />
 	</React.StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
